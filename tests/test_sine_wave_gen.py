40825a16bab695dd66d45f5bec5da297ccd185aa
Gaspar Santamarina <gsantamarina@innova-space.com>
2022-10-02 10:21:43 -0300

    Fix simulation

--- tests/test_sine_wave_gen.py
+++ tests/test_sine_wave_gen.py
@@ -6,15 +6,16 @@ from cocotb.triggers import Edge, RisingEdge, Timer
 @cocotb.test()
 async def test_sine_wave_generator(dut) -> None:
 
-    dut.wave_period.value = 2
-
-    dut.reset.value = 1
-    await Timer(1, units="us")
-    dut.reset.value = 0
-    await Timer(1, units="us")
+    dut.wave_period.value = 0
 
     # Create and start clcok
     clock = Clock(dut.CLOCK_100, 1, units="us")
     cocotb.start_soon(clock.start())
 
-    await Timer(100, units="us")
+    dut.reset.value = 0
+    await Timer(1, units="us")
+    dut.reset.value = 1
+    await Timer(1, units="us")
+
+
+    await Timer(100, units="ms")
